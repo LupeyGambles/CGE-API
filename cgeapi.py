2467de44dad2b6461df51d1f9c6b1d5737a6b4a4
Lupey <monkerunkeVR@gmail.com>
2025-10-24 23:10:02 -0500

    Update cgeapi.py

--- cgeapi.py
+++ cgeapi.py
@@ -36,7 +36,7 @@ def get_server_info():
                 "game": getattr(info, "game", "unknown"),
                 "version": getattr(info, "version", "unknown"),
                 "password_protected": True,
-                "players_list": players_list
+                "players_list":"password protected"
             })
 
         # Query players safely
